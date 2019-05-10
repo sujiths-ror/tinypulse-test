049c1af8f5bada91d584e8c538654af5f66c26f6
sujiths-ror <sujith.s@techversantinc.com>
2019-05-10 15:42:08 +0530

    Pomotion model methods added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :promotions
+  has_many :promotions, dependent: :destroy
 
   validates :title, presence: true
   validates :description, presence: true
--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -3,4 +3,14 @@ class Promotion < ApplicationRecord
 
   validates :title, presence: true
   validates :promotion_type, presence: true
+
+  PROMOTION_TYPE = {single: 1, multiple: 2}
+
+  def single?
+    promotion_type == PROMOTION_TYPE[:single]
+  end
+
+  def multiple?
+    promotion_type == PROMOTION_TYPE[:multiple]
+  end
 end
